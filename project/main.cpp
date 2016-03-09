1fc746d0683fe7d17d923b46ae5b23670f946808
Nikitina-Ann <annet-girl@mail.ru>
2016-03-09 11:19:34 +0300

    Full test coverage

--- project/main.cpp
+++ project/main.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <omp.h>
 using namespace std;
 #define SIZE 100 //максимальный размер матриц
-//int r1, c1, r2, c2; //размерности матриц a, b
+struct timeval tv1,tv2,dtv;
+
+long int dtime(){
+  dtv.tv_sec= tv2.tv_sec -tv1.tv_sec;
+  dtv.tv_usec=tv2.tv_usec-tv1.tv_usec;
+  if(dtv.tv_usec<0) { dtv.tv_sec--; dtv.tv_usec+=1000000; }
+  return dtv.tv_sec*1e+6+dtv.tv_usec;
+}
+
 //Умножение матриц
 void mult(int a[SIZE][SIZE], int b[SIZE][SIZE], int res[SIZE][SIZE], int r1, int c1, int c2) {
     for(int i=0; i<r1; ++i)
-    for(int j=0; j<c2; ++j)
-    for(int k=0; k<c1; ++k)
+        for(int j=0; j<c2; ++j)
+            res[i][j]=0;
+    #pragma omp parallel 
     {
-        res[i][j]+=a[i][k]*b[k][j];
+	  #pragma omp for
+    for(int i=0; i<r1; ++i)
+        for(int j=0; j<c2; ++j)
+            for(int k=0; k<c1; ++k)
+                res[i][j]+=a[i][k]*b[k][j];
     }
 }
 //Чтение элементов матрицы
-void readMatrix(int a[SIZE][SIZE], int *r, int *c, char* filename) {
+void readMatrix(int a[SIZE][SIZE], int* r, int* c, char* filename) {
     FILE *in=fopen(filename, "rt");//файл чтения
     fscanf(in,"%d %d", r, c); //Считали размерности
-    for(int i=0;i<*c;i++)
+    for(int i=0;i<*r;i++)
     {
-     for(int j=0;j<*r;j++)
+     for(int j=0;j<*c;j++)
        fscanf(in,"%d",&a[i][j]);
     }
     fclose(in);
 }
+
 void writeMatrix(int res[SIZE][SIZE], int r, int c){
     FILE *out=fopen("files/output.txt", "wt");//файл записи
+    fprintf(out,"%d %d\n", r, c);
     for(int i=0; i<r; ++i)
         for(int j=0; j<c; ++j)
         {
@@ -34,10 +52,11 @@ void writeMatrix(int res[SIZE][SIZE], int r, int c){
         }
     fclose(out);
 }
+
 int main()
 {
     int r1, c1, r2, c2; //размерности матриц a, b
-    int a[SIZE][SIZE], b[SIZE][SIZE], res[SIZE][SIZE];
+    int a[SIZE][SIZE], b[SIZE][SIZE], res[SIZE][SIZE], res1[SIZE][SIZE];
     //Чтение элементов матрицы
     readMatrix(a, &r1, &c1, "files/a.txt");
     readMatrix(b, &r2, &c2, "files/b.txt");
@@ -45,17 +64,13 @@ int main()
         cout << endl << "Количество столбцов первой матрицы не равно количеству строк второй матрицы" << endl;
         return -1;
     }
+    struct timezone tz;
+    gettimeofday(&tv1, &tz); 
     //Умножение матриц
     mult(a, b, res, r1, c1, c2);
+    gettimeofday(&tv2, &tz);
+    printf("Время умножения: %ld мкрс\n", dtime());
+    //Запись результата в файл
     writeMatrix(res, r1, c2);
-    cout << endl << "Результат:" << endl;
-    for(int i=0; i<r1; ++i){
-        for(int j=0; j<c2; ++j)
-        {
-            cout << " " << res[i][j];
-            if(j==c2-1)
-                cout << endl;
-        }
-    }
     return 0;
 }
--- project/tests/testclass.cpp
+++ project/tests/testclass.cpp
@@ -13,10 +13,7 @@
 #include <iostream>
 #include "testclass.h"
 #define SIZE 100 
-
-
-//int r1, c1, r2, c2;
-
+#define NUMBER_TEST 5
 CPPUNIT_TEST_SUITE_REGISTRATION(testclass);
 
 testclass::testclass() {
@@ -32,45 +29,71 @@ void testclass::tearDown() {
 }
 
 void readMatrix(int a[SIZE][SIZE], int *r, int *c, char* filename);
-
-void testclass::testReadMatrix() {
-    int** a;
-    int** b;
-    //readMatrix(a, b);
-    /*if (true ) {
-        CPPUNIT_ASSERT(false);
-    }*/
+void testclass::testReadMatrix(){
+    const int r=5, c=6;
+    int test[r][c] = {{1, 2, 3, 4, 5, 6}, 
+                      {3, 2, 1, 9, 8, 7}, 
+                      {4, 5, 6, 7, 8, 9},
+                      {2, 1, 2, 1, 2, 1}, 
+                      {2, 2, 2, 2, 2, 2}};
+    int r1, c1;
+    int a[SIZE][SIZE];
+    readMatrix(a, &r1, &c1,  "files/a.txt");
+    if(c!=c1 || r!=r1)
+            CPPUNIT_ASSERT(false);
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++){
+            if (test[i][j]!=a[i][j])
+                CPPUNIT_ASSERT(false);
+        }
+    CPPUNIT_ASSERT(true);
 }
 
 void writeMatrix(int res[SIZE][SIZE], int r, int c);
-
-void testclass::testWriteMatrix() {
-    int** res;
-    //writeMatrix(res);
-    /*if (true ) {
-        CPPUNIT_ASSERT(false);
-    }*/
+void testclass::testWriteMatrix(){
+    const int r=5, c=6;
+    int test[SIZE][SIZE] = {{1, 2, 3, 4, 5, 6}, 
+                            {3, 2, 1, 9, 8, 7}, 
+                            {4, 5, 6, 7, 8, 9},
+                            {2, 1, 2, 1, 2, 1}, 
+                            {2, 2, 2, 2, 2, 2}};
+    writeMatrix(test, r, c);
+    int r1, c1;
+    int a[SIZE][SIZE];
+    readMatrix(a, &r1, &c1,  "files/output.txt");
+    if(c!=c1 || r!=r1)
+            CPPUNIT_ASSERT(false);
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++){
+            if (test[i][j]!=a[i][j])
+                CPPUNIT_ASSERT(false);
+        }
+    CPPUNIT_ASSERT(true);
+    
 }
 
 void mult(int a[SIZE][SIZE], int b[SIZE][SIZE], int res[SIZE][SIZE], int r1, int c1, int c2);
-
 void testclass::testMult() {
-    int r1, c1, r2, c2;
+    int r1, c1, r2, c2, r, c;
     int a[SIZE][SIZE], b[SIZE][SIZE], out[SIZE][SIZE], res[SIZE][SIZE];
-    readMatrix(a, &r1, &c1, "files/a.txt");
-    readMatrix(b, &r2, &c2, "files/b.txt");
-    /*if(c1!=r2)
-        CPPUNIT_ASSERT(false);*/
-    mult(a, b, out, r1, c1, c2);
-    int c,r;
-    readMatrix(res, &r, &c, "files/res.txt");
-    writeMatrix(out, r1, c2);
-    if(c!=c2 || r!=r1)
-        CPPUNIT_ASSERT(false);
-    for(int i=0;i<r1;i++)
-    for(int j=0;j<c2;j++){
-        if (out[i][j]!=res[i][j])
-          CPPUNIT_ASSERT(false);
+    char filename_a[SIZE], filename_b[SIZE], filename_res[SIZE];
+    for (int num =1;num<NUMBER_TEST+1;num++){
+        sprintf(filename_a, "files/%d/a.txt", num);
+        sprintf(filename_b, "files/%d/b.txt", num);
+        sprintf(filename_res, "files/%d/res.txt", num);
+        readMatrix(a, &r1, &c1, filename_a);
+        readMatrix(b, &r2, &c2, filename_b);
+        if(c1!=r2)
+            CPPUNIT_ASSERT(false);
+        mult(a, b, out, r1, c1, c2);
+        readMatrix(res, &r, &c, filename_res);
+        if(c!=c2 || r!=r1)
+            CPPUNIT_ASSERT(false);
+        for(int i=0;i<r1;i++)
+        for(int j=0;j<c2;j++){
+            if (out[i][j]!=res[i][j])
+                CPPUNIT_ASSERT(false);
+        }
     }
     CPPUNIT_ASSERT(true);
 }
